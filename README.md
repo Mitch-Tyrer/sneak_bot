7102001f08ebaf86d727d3fd4a15b637f8a23d05
Mitch Tyrer <mltcinematography@gmail.com>
2021-08-16 22:43:56 -0600

    started working on the automated form fill. Unable to correctly select billing inputs

--- README.md
+++ README.md
@@ -2,9 +2,14 @@
 
 # Description
 
-This is code for automated online shopping with the ability to search for products based on key terms, find the correct link to that product and then automatically purchase it.
+This is code for automated online shopping for shoes with the ability to search for products based on key terms, find the correct link to that product and then automatically purchase it.
 
-Currently this code only works for online webstores utilizing the Shopify API
+Currently this code only works for online webstores utilizing the Shopify API.  Built using the Selenium-Python library and the chromium webdriver for automation of form entry.
+
+Is capable of inputing data currently until it reaches the billing information.  Currently working out how to use selenium to reach the proper inputs as they are in an iframe
+
+# Note
+This is currently built around a random shopify site I was using to test.  If you want to customize this code you will have to manually modify the xpaths to the html inputs for the shopify site you are using.
 
 # Getting Started
 
@@ -32,5 +37,26 @@ At the moment this code only requires the following dependencies to run:
 
 ```
 pip install requests
+pip install selenium
 
 ```
+
+#Using Selenium.
+Please visit `https://selenium-python.readthedocs.io/index.html` for more information about this python plugin.
+
+In order to run this script you will need to install one of the following drivers related to the version of your preferred web browser.
+
+```
+Chrome:	https://sites.google.com/a/chromium.org/chromedriver/downloads
+Edge:	https://developer.microsoft.com/en-us/microsoft-edge/tools/webdriver/
+Firefox:	https://github.com/mozilla/geckodriver/releases
+Safari:	https://webkit.org/blog/6900/webdriver-support-in-safari-10/
+```
+
+# How to use.
+Before anything else modify the `user_info.txt` file with your preferred credentials.  The formatting is very strict so only change the information, leave the keys the same and the spacing around the colons is intentionally.
+
+   * Your modified file values should look like this:
+         ```
+         email : example@example.com
+         ```
--- shopify.py
+++ shopify.py
@@ -1,25 +1,29 @@
 # import libraries/dependences
 
+from os import stat
 import requests
 import json
+import time
+from selenium import webdriver
+from selenium.webdriver.support.ui import Select, WebDriverWait
 
 # import credentials from external text file and convert to a dictionary [include all form elements from a shopify checkout]
-options = {}
+user_info = {}
 #url = input("What shopify store do you want to search? ")
 #search = input("What are you searching for? ")
 
-url = "https://www.allbirds.com/"
-#search = "Men's Tree Dashers - Kilimanjaro (Light Green Sole)"
 
-f = open('options.txt', 'r')
+
+f = open('user_info.txt', 'r')
 for line in f:
-    (key, val) = line.strip().split(':')
-    options[key] = val
+    (key, val) = line.strip().split(' : ')
+    user_info[key] = val
 
 # Function that searches for key terms in a json product list
 
+print(user_info)
 
-def searc_products():
+def search_products():
     usr_query = input("What type of product are you looking for? ")
     r = requests.get(f"{url}products.json")
     products = json.loads((r.text))['products']
@@ -49,21 +53,76 @@ def searc_products():
 # Function to Check if item is available by searching throught the products json (shopname.com/products.json)
 # Do this by comparing the desired user input against the 'title' in the json
 # append the 'handle' to the url (shopname.com/products)
+url = "https://www.allbirds.com/"
+search = "Women's Tree Breezers - Everest (Dark Green Sole)"
 
 def check_availability(url, search):
     r = requests.get(f"{url}products.json")
     products = json.loads((r.text))['products']
-
     for product in products:
+        print(product['title'])
         product_name = product['title']
 
         if product_name == search:
-            product_url = f"{url}products/{product['handle']}"
+            print(product_name)
+            product_url = f"{url}products/{product['handle']}"            
             return product_url
-    return False
+    else:
+        return False
 
 
 # If available open the site select size (may need to add a way to search for inputs for buttons and selects as well as a name/label for size)
 # Click add to cart
 # Click checkout
 # fill in form with information from dictionary
+
+
+test_url = check_availability(url, search)
+driver = webdriver.Chrome(executable_path=r'C:\Users\necro\Documents\Coding\Selenium_BrowserDrivers\chromedriver.exe')
+driver.get(test_url)
+
+
+driver.find_element_by_xpath('//button[@aria-label="Add Size 10"]').click()
+driver.find_element_by_xpath('//button[@id="add-to-cart"]').click()
+time.sleep(5)
+driver.find_element_by_xpath('//a[@href="/checkout?locale=en-US"]').click()
+time.sleep(5)
+driver.find_element_by_xpath('//input[@placeholder="Email"]').send_keys(user_info['email'])
+time.sleep(1)
+driver.find_element_by_xpath('//input[@placeholder="First name"]').send_keys(user_info['first_name'])
+time.sleep(1)
+driver.find_element_by_xpath('//input[@placeholder="Last name"]').send_keys(user_info['last_name'])
+time.sleep(1)
+driver.find_element_by_xpath('//input[@placeholder="Address"]').send_keys(user_info['address'])
+time.sleep(1)
+driver.find_element_by_xpath('//input[@placeholder="City"]').send_keys(user_info['city'])
+time.sleep(1)
+select = Select(driver.find_element_by_xpath('//select[@placeholder="State"]'))
+select.select_by_value(user_info['state'])
+time.sleep(1)
+driver.find_element_by_xpath('//input[@placeholder="ZIP code"]').send_keys(user_info['zip'])
+time.sleep(1)
+driver.find_element_by_xpath('//button[@id="continue_button"]').click()
+time.sleep(5)
+driver.find_element_by_xpath('//button[@id="continue_button"]').click()
+time.sleep(10)
+
+#billing informations
+driver.find_element_by_xpath('//iframe[@class="card-fields-iframe"]').send_keys(user_info['card_num'])
+time.sleep(1)
+driver.find_element_by_xpath('//input[@id="name"]').send_keys(f"{user_info['first_name']} {user_info['last_name']}")
+time.sleep(1)
+driver.find_element_by_xpath('//input[@id="expiry"]').send_keys(user_info['exp_date'])
+time.sleep(1)
+driver.find_element_by_xpath('//input[@id="verification_value"]').send_keys(user_info['sec_code'])
+time.sleep(1)
+driver.find_element_by_xpath('//button[@id="continue_button"]').click()
+
+
+
+# way to interate over and find the value tags in a html select
+# select = driver.find_element_by_xpath('//select[@placeholder="State"]')
+# states = select.find_elements_by_tag_name("option")
+# stateList = []
+# for state in states:
+#     stateList.append(state.get_attribute("value"))
